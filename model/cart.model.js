1dce135d03c548f277b77cda66711581633b1f9c
bautistaprivitera <juanbautistaprivitera@gmail.com>
2026-01-20 20:23:40 -0300

    primer push

---
+++ model/cart.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema({
+    products: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Products',
+                required: true
+
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                default: 1
+            }
+        }
+    ]
+})
+
+export default mongoose.model('Carts', cartSchema)
---
+++ model/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const studenSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    cart: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'carts'
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+});
+
+export default mongoose.model('Users', studenSchema);
